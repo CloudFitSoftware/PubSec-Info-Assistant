1650929c90e3719cc6f9d7b9c41446a5c98d892a
baronbcb <86253657+baronbcb@users.noreply.github.com>
2024-06-28 12:28:30 -0400

    enable_rbac_authorization kv

--- infra/core/security/keyvault/keyvault.tf
+++ infra/core/security/keyvault/keyvault.tf
@@ -11,6 +11,7 @@ resource "azurerm_key_vault" "kv" {
   soft_delete_retention_days      = 7
   purge_protection_enabled        = true
   public_network_access_enabled   = false
+  enable_rbac_authorization       = true
   network_acls {
     default_action = "Deny"
     bypass         = "AzureServices"
