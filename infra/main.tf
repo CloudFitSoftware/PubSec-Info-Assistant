b43436c0acc055e829dcae846e3ee0e690de004f
baronbcb <86253657+baronbcb@users.noreply.github.com>
2024-07-05 16:51:17 -0400

    typo

--- infra/main.tf
+++ infra/main.tf
@@ -541,8 +541,8 @@ data "azurerm_virtual_network" "existing" {
 
 data "azurerm_subnet" "existing" {
   name                 = "kv"
-  virtual_network_name = data.azurerm_virtual_network.existing.vnet.name
-  resource_group_name  = data.azurerm_virtual_network.existing.vnet.resource_group_name
+  virtual_network_name = data.azurerm_virtual_network.existing.name
+  resource_group_name  = data.azurerm_virtual_network.existing.resource_group_name
 }
 
 module "aks" {
